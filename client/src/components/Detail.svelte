562e0c77b8132d97a9141f156827d54863ccbb12
yunemk <yunemk.899@gmail.com>
2021-08-18 16:50:51 +0900

    Added detail page markups
    a

--- client/src/components/Detail.svelte
+++ client/src/components/Detail.svelte
@@ -1,1 +1,58 @@
-<div />
+<script>
+  import { createEventDispatcher, onMount } from "svelte";
+  import Image from "./Image.svelte";
+
+  const loadDetails = async () => {};
+
+  export let place;
+
+  const dispatch = createEventDispatcher();
+
+  const restoreReviewing = () => dispatch("restore-reviews");
+
+  const loadNearbyPlaces = async () => {
+    const data = await fetch(
+      `api/places/?longitude=${place.longitude}&latitude=${place.latitude}`
+    );
+    return await data.json();
+  };
+
+  onMount(() => {
+    const coord = [parseFloat(place.longitude), parseFloat(place.latitude)];
+    const map = L.map("map").setView(coord, 13);
+    L.marker(coord).addTo(map);
+  });
+</script>
+
+<div class="mx-auto">
+  <Image src={place.thumbnail} alt={place.name} />
+</div>
+<div>
+  <h1 class="text-2xl">{place.name}</h1>
+  <code>{place.address}</code>
+  <p>{place.description}</p>
+</div>
+<div id="map">map</div>
+{#await loadNearbyPlaces()}
+  <div>loading...</div>
+{:then res}
+  <div>
+    <h2>近くの観光地</h2>
+    {#if res.success}
+      <ul>
+        {#each res.data as nearbyPlace (nearbyPlace.id)}
+          <li>{nearbyPlace.name} - {`${parseInt(nearbyPlace.distance)}m`}</li>
+        {/each}
+      </ul>
+    {:else}
+      <div>Internal server error occurred.</div>
+    {/if}
+  </div>
+{/await}
+<button on:click={restoreReviewing}>他の場所を探す</button>
+
+<style>
+  #map {
+    height: 200px;
+  }
+</style>
