084425006cb6cec8c38cf285ae1c81aace9bdc77
yunemk <yunemk.899@gmail.com>
2021-08-18 13:44:25 +0900

    Extracted button components

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,7 +1,10 @@
 <script>
-  import { onMount } from "svelte";
+  import BtnDislike from "./components/BtnDislike.svelte";
+  import BtnLike from "./components/BtnLike.svelte";
+
   let places = [];
   let curIdx = 0;
+  let success = false;
 
   const like = () => {
     // unimplemented
@@ -11,10 +14,12 @@
     curIdx = (curIdx + 1) % (len === 0 ? 1 : len);
   };
 
-  onMount(async () => {
-    const data = await fetch("api/places");
-    places = await data.json();
-  });
+  const loadPlaces = async () => {
+    const res = await fetch("api/places");
+    const data = await res.json();
+    success = data.success;
+    places = data.data;
+  };
 </script>
 
 <div class="grid grid-rows-8 h-screen">
@@ -23,33 +28,31 @@
     id="place"
     class="row-span-6 grid grid-rows-5 gap-y-3 p-3 md:w-2/5 md:mx-auto "
   >
-    {#if places[curIdx]}
-      <div class="row-span-3 mx-auto">
-        <img
-          class="h-full"
-          src={places[curIdx].thumbnail}
-          alt={places[curIdx].name}
-        />
-      </div>
-      <div>
-        <h1 class="text-3xl">{places[curIdx].name}</h1>
-        <p class="line-clamp-2">{places[curIdx].description}</p>
-      </div>
-      <div class="flex justify-around items-center md:w-1/2 md:mx-auto">
-        <button
-          id="dislike"
-          class="h-16 w-16 bg-blue-400 rounded-full"
-          on:click={dislike}>d</button
-        >
-        <button
-          id="like"
-          class="h-16 w-16 bg-green-400 rounded-full"
-          on:click={like}>l</button
-        >
-      </div>
-    {:else}
-      <div class="row-span-5 text-center">Loading...</div>
-    {/if}
+    {#await loadPlaces()}
+      <p>Loading...</p>
+    {:then}
+      {#if success}
+        <div class="row-span-3 mx-auto">
+          <img
+            class="h-full"
+            src={places[curIdx].thumbnail}
+            alt={places[curIdx].name}
+          />
+        </div>
+        <div>
+          <h1 class="text-2xl">{places[curIdx].name}</h1>
+          <p class="line-clamp-2">{places[curIdx].description}</p>
+        </div>
+        <div class="flex justify-around items-center md:w-1/2 md:mx-auto">
+          <BtnDislike on:click={dislike} />
+          <BtnLike on:click={like} />
+        </div>
+      {:else}
+        <div class="row-span-5 text-center">Server error</div>
+      {/if}
+    {:catch error}
+      <p style="color: red">{error.message}</p>
+    {/await}
   </main>
   <footer class="bg-gray-200" />
 </div>
---
+++ client/src/components/BtnDislike.svelte
@@ -0,0 +1,3 @@
+<button id="dislike" class="h-16 w-16 bg-blue-400 rounded-full" on:click
+  >d</button
+>
