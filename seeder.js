28809edc7efaab2a166f4b8869d696b6fec33032
MisonoTakezo <misonotakezo1213@gmail.com>
2021-08-19 13:33:06 +0900

    fix sqlite insert

--- seeder.js
+++ seeder.js
@@ -3,9 +3,14 @@ import db from "./setupDB.js";
 const places = JSON.parse(Deno.readTextFileSync("./_data/data.json"));
 
 places.forEach((place) => {
-  db.query(`INSERT INTO places (name, description, address, longitude, latitude, thumbnail) VALUES (${place.name}, ${place.description}, ${place.address}, ${place.longitude}, ${place.latitude}, ${place.tumbnail})`);
+  db.query(
+    `INSERT INTO places (name, description, address, longitude, latitude, thumbnail) VALUES (
+      (?), (?), (?), (?), (?), (?)
+    )`,
+    [place.name, place.description, place.address, place.longitude, place.latitude, place.thumbnail]
+  );
 })
 
-for (const [place] of db.query("SELECT * FROM places")) {
-  console.log(place);
-}
+
+let answer = db.query("SELECT * FROM places");
+console.log(answer);
