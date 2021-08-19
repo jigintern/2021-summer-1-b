2e209ceaf38b65c5b69ecf92d85ea760d7373bec
MisonoTakezo <misonotakezo1213@gmail.com>
2021-08-19 11:46:22 +0900

    db seeds

--- seeder.js
+++ seeder.js
@@ -1,14 +1,10 @@
-import { db } from "./setupDB.js";
+import db from "./setupDB.js";
 
 const places = JSON.parse(Deno.readTextFileSync("./_data/data.json"));
 
 places.forEach((place) => {
-  db.query(`INSERT INTO places (name, description, address, longitude, latitude, thumbnail) VALUES (${place.name}, ${place.description}, ${place.adress}, ${place.longitude}, ${place.latitude}, ${place.tumbnail})`);
+  db.query(`INSERT INTO places (name, description, address, longitude, latitude, thumbnail) VALUES (${place.name}, ${place.description}, ${place.address}, ${place.longitude}, ${place.latitude}, ${place.tumbnail})`);
 })
-
-for (const name of names) {
-  db.query("INSERT INTO places VALUES ()", [name]);
-}
 
 for (const [place] of db.query("SELECT * FROM places")) {
   console.log(place);
